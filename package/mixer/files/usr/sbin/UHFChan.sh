e3b10df989af4585048ad714c49906634986084b
Daniel de Kock <daniel@riot.network>
2019-02-26 15:23:07 +0200

    Updated gain array for constant power with new RF PA board, added Yun ATH79 config

--- package/mixer/files/usr/sbin/UHFChan.sh
+++ package/mixer/files/usr/sbin/UHFChan.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-# set the Wifi channel and Mixer LO so that we can use 26 TVWS channels while the 2.4GHs filter will only allow 13 to pass
-# arguments are : -c TV channel [21 - 26] : corresponsds to ITU band IV channles ( 470- 694 MHz )
-#		: -b bandwidth [5,10,20] MHz
+# set the Wifi channel and Mixer LO so that we can use 28 TVWS channels while the 2.4GHs filter will only allow 13 to pass
+# arguments are :  TV channel [21 - 48] : corresponsds to ITU band IV channles ( 470- 694 MHz )
 #
 # effects	: sets chanbw and channel parameters for radio0 ( using UCI )
-#		: sets mixer LO by setting config file "freq" to 1918 for lower (12-34) or "freq" to 1814 for upper (35-47) TV channels ( using UCI )
+#		: sets mixer LO by setting config file "freq" to required LO frequency for teh up/downconversion
 # TODO call form LuCi in the web interface/use a config file and use  procd so we can use it from UCI / LuCI.
 
 
 LOWEST_UHF=21		# cannot set to channels lower than this
-HIGHEST_UHF=48
+HIGHEST_UHF=31		# highest TVWS is 48, highest supported by PA is 31
 UHF_OFFSET=20	 	# we start at UHF channel 21
 
-# gain settings for atheros to have a constant-ish output - channel 21 .. 48 // unused for now
-GAINARRAY=" 4 4 4 4 5 4 4 4 3 3 3 3 3 7 7 7 7 5 3 3 2 1 1 1 1 2 2 2 " 
+# max gain settings for atheros to have a constant-ish output @26-28 dBm- channel 21 .. 48 // unused for now
+GAINARRAY=" 8 8 8 8 8 8 10 10 10 12 12 12 12 12 12 12 12 12 1 1 1 1 1 1 1 1 1 1 " 
 
 # mixer settings for atheros to have a constant channel (7, 2442 MHz) setting - channel 21 .. 48 - Standard Wifi configuration
 MIXERARRAY="1968 1960 1952 1944 1936 1928 1920 1912 1904 1896 1888 1880 1872 1864 1856 1848 1840 1832 1824 1816 1808 1800 1792 1784 1776 1768 1760 1752" 
 CONSTCHANNEL=7
 CONSTCHANNELFREQ=2442
-CONSTGAIN=1
+CONSTGAIN=8 # gain set at a constant - some resets still happen in ath9k when we change channel settings frequently
 
 
 
@@ -52,7 +51,7 @@ vary_mixer_for_Channel()
 get_UHF_Freq()
 {
 	local UHFFreq	
-		#calculate the UHF cnahhel frequency based on mixer value and constant channel frequency
+		#calculate the UHF channel frequency based on mixer value and constant channel frequency
 	UHFFreq=`expr $CONSTCHANNELFREQ - $1`
 	# "return"
 	echo $UHFFreq
@@ -82,6 +81,7 @@ else :
 
 	MIXER_SETTING=$(vary_mixer_for_Channel $1)
 	WIFI_SETTING=$CONSTCHANNEL
+	# WIFI_POWER=$(calc_power_for_Channel $1)
 	WIFI_POWER=$CONSTGAIN
 	UHFFREQ=$(get_UHF_Freq $MIXER_SETTING)
 
